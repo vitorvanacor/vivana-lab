e365762d833bafd9f3a8a440a6ff6776f708353b
vitorvanacor <vitor_vanacor@hotmail.com>
2021-10-31 00:06:07 -0300

    fix: favicon not found

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,9 @@
+<script context="module">
+  import { base as basePath } from '$app/paths';
+</script>
+
+<svelte:head>
+	<link rel="icon" href={`${basePath}/favicon.png`} />
+</svelte:head>
+
+<slot />
