2c656b634deb59b3dd06d2346b734c0a83902459
vitorvanacor <vitor_vanacor@hotmail.com>
2021-11-15 17:49:04 -0300

    dev: move eslintrc and prettierrc to .devconfig

---
+++ .devconfig/prettier.cjs
@@ -0,0 +1,8 @@
+// Using CommonJS because VSCode does not support ES6 modules
+module.exports = {
+  tabWidth: 2,
+  useTabs: false,
+  trailingComma: 'all',
+  singleQuote: true,
+  quoteProps: 'consistent',
+};
